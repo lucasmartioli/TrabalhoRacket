1ec990bf17d60fc5ef220cf8be2414f4fd27750f
eduardo villas boas dos santos <eduardo.dabrisa@gmail.com>
2015-08-30 19:01:27 -0300

    5.1 terminado

--- Lista5.rkt
+++ Lista5.rkt
@@ -3,6 +3,22 @@
 (require rackunit)
 (require rackunit/text-ui)
 
+(define >?-tests
+  (test-suite
+   ">? tests"
+   (check-equal? (>? 0 0) #f)
+   (check-equal? (>? 0 2) #f)
+   (check-equal? (>? 2 0) #t)
+   (check-equal? (>? 2 3) #f)
+   (check-equal? (>? 4 3) #t)))
+
+(define (>? a b)
+  (cond
+    [(zero? a) #f]
+    [(zero? b) #t]
+    [else (>? (sub1 a) (sub1 b))]))
+
+
 (define <?-tests
   (test-suite
    "<? tests"
@@ -18,6 +34,53 @@
     [(zero? a) #t]
     [else (<? (sub1 a) (sub1 b))]))
 
+;;>=
+(define <=?-tests
+  (test-suite
+   "<=? tests"
+   (check-equal? (<=? 0 0) #t)
+   (check-equal? (<=? 0 2) #t)
+   (check-equal? (<=? 2 0) #f)
+   (check-equal? (<=? 2 3) #t)
+   (check-equal? (<=? 4 3) #f)))
+
+(define (<=? a b)
+  (cond 
+    [(>? a b) #f]
+    [else #t]) 
+ )
+
+(define >=?-tests
+  (test-suite
+   ">=? tests"
+   (check-equal? (>=? 0 0) #t)
+   (check-equal? (>=? 0 2) #f)
+   (check-equal? (>=? 2 0) #t)
+   (check-equal? (>=? 2 3) #f)
+   (check-equal? (>=? 4 3) #t)))
+
+(define (>=? a b)
+  (cond 
+    [(<? a b) #f]
+    [else #t]) 
+ )
+
+(define =?-tests
+  (test-suite
+   "= tests"
+   (check-equal? (=? 0 0) #t)
+   (check-equal? (=? 0 2) #f)
+   (check-equal? (=? 2 0) #f)
+   (check-equal? (=? 2 3) #f)
+   (check-equal? (=? 3 3) #t)
+   (check-equal? (=? 4 3) #f)))
+
+(define (=? a b)
+  (cond 
+    [(<? a b) #f]
+    [(>? a b) #f]
+    [else #t]) 
+ )
 
 
 (define (executa-testes . testes)
@@ -25,4 +88,4 @@
   (void))
 
 ;; Chama a função para executar os testes.
-(executa-testes <?-tests)
+(executa-testes <?-tests >?-tests <=?-tests >=?-tests =?-tests)
