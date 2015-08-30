f975349494e02bf4ef3109002a034379e1f7477d
eduardo villas boas dos santos <eduardo.dabrisa@gmail.com>
2015-08-30 19:51:56 -0300

    implementado o exercicio 5.3

--- Lista5.rkt
+++ Lista5.rkt
@@ -82,10 +82,27 @@
     [else #t]) 
  )
 
+(define drop-tests
+  (test-suite
+   "drop tests"
+   (check-equal? (drop (list 1 2 3) 0) (list 1 2 3))
+   (check-equal? (drop (list 3 6 8 9) 2) (list 8 9))
+   (check-equal? (drop (list 3 6 8 9) 4) empty)
+   (check-equal? (drop (list 6 8 9) 4) empty)
+   (check-equal? (drop empty 0) empty)
+   (check-equal? (drop empty 2) empty)))
+
+(define (drop list1 n)
+  (cond 
+    [(zero? n) list1]
+    [(empty? list1) empty]
+    [else (drop (rest list1) (sub1 n))]
+    )
+ )
 
 (define (executa-testes . testes)
   (run-tests (test-suite "Todos os testes" testes))
   (void))
 
 ;; Chama a função para executar os testes.
-(executa-testes <?-tests >?-tests <=?-tests >=?-tests =?-tests)
+(executa-testes <?-tests >?-tests <=?-tests >=?-tests =?-tests drop-tests)
